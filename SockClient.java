ec7d419b9133c56e9fe312dded831f146d908a6a
itwm <135006027+itwm@users.noreply.github.com>
2023-05-31 12:16:35 +0200

    Klient SockClient

---
+++ SockClient.java
@@ -0,0 +1,52 @@
+/**
+ *
+ *  @author itwm & ChatGPT
+ */
+
+
+import java.io.*;
+import java.net.Socket;
+import java.io.File;
+import java.util.Scanner;
+
+
+public class SockClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = null;
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj adres IP serwera : ");
+        String serverAddress = scanner.nextLine();
+        System.out.print("Podaj port na ktorym nasłuchuje serwer : ");
+        int serverPort = Integer.parseInt(scanner.nextLine());
+        try {
+            // Utworzenie połączenia z serwerem
+            socket = new Socket(serverAddress, serverPort);
+            System.out.print("Podaj ścieżkę do pliku z nazwa: ");
+            String filePath = scanner.nextLine();
+            File file = new File(filePath);
+                if (file.exists()) {
+                bis = new BufferedInputStream(new FileInputStream(file));
+                bos = new BufferedOutputStream(socket.getOutputStream());
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                    while ((bytesRead = bis.read(buffer)) != -1) {
+                        bos.write(buffer, 0, bytesRead);
+                    }   
+                System.out.println("Plik został pomyślnie wysłany.");
+                } else {
+                    System.out.println("Plik nie istnieje.");
+                } 
+            } finally {
+            // Zamknięcie strumieni i połączenia
+            if (bis != null)
+                bis.close();
+            if (bos != null)
+                bos.close();
+            if (socket != null)
+                socket.close();
+        }
+       
+    }
+}
